97af30d845348c9f329e3019322a5e2a0e6bb95f
irfan ayaz <byirfanayaz@gmail.com>
2024-02-21 23:08:04 +0300

    Birinci Hafta

---
+++ Intro/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using Intro.Business;
+using Intro.Entities;
+
+
+
+//Course[] courses = {course1, course2, course3};
+
+
+CourseManager courseManager = new CourseManager();
+Course [] courses2 = courseManager.GetAll();
+for (int i = 0; i < courses2.Length; i++)
+{
+    Console.WriteLine(courses2[i].Name + " / " + courses2[i].Price);
+}
+
+
+
+
+
+
+
+
